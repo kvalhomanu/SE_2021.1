a6c7f0e9d6d59351780b049cc6a49c56cf27e4bf
Marcelo Carvalho <32714498+kvalhomanu@users.noreply.github.com>
2021-10-20 21:30:55 -0300

    Update Questão_Tinkercad.ino

--- Questão_Tinkercad.ino
+++ Questão_Tinkercad.ino
@@ -5,6 +5,7 @@
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);  // Pinos LCD
 const int pot = 0;          // pino de entrada do potenciômetro
 int tensaoPot = 0;			// variavel para armazenar valor do potenciometro
+float tensaoV = 0;      // variavel para armazenar valor convertido
 
 void setup() {
   lcd.begin(16, 2);					// Inicialização de LCD 16x2
@@ -12,9 +13,12 @@ void setup() {
 }
 
 void loop() {
-  lcd.setCursor(0, 1);					//Setando posição inicial do cursor no LCD
+  lcd.setCursor(0, 1);					// Setando posição inicial do cursor no LCD
   tensaoPot = analogRead(pot);  		// recebe valor do potenciometro
+  tensaoV = (tensaoPot * 5.0) / 1023,0; // converte o valor analogico em digital
   lcd.print("Tensao: ");                
-  lcd.print(tensaoPot);                   // mostrando tensao no display
+  lcd.print(tensaoV);                   // mostrando tensao no display
+  lcd.print("V");
   delay(100);                          // tempo para nova leitura
 }
+
